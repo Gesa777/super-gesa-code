b70ead41d5b9edfa65637e092b42d97170a60b14
Baczyk <110850248+Gesa777@users.noreply.github.com>
2023-06-18 05:33:44 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,67 +9,45 @@ import numpy as np
           ###                            ###
           ##################################
 
-
-
-
-#max(a,b): ####################
-#
-#Arguments :
-#a -- a real number
-#b -- a real number
-#
-#Return :
-#The maximum of (a;b)
-
+"""
+max
+Arguments : a, b two real numbers
+Return : the maximum of (a;b)
+"""
 def max(a,b):
     if a >= b:
         return a
     else :
         return b
 
-#sig1(x): ####################
-#
-#Arguments :
-#x -- a real number
-#
-#Return :
-#The basic sigmoid function applied to x
-
+"""
+sig1
+Arguments : x a real number
+Return : the basic sigmoid function applied to x
+"""
 def sig1(x):
     return 1/(1+exp(-x))
 
-#sig(x,k): ####################
-#
-#Arguments :
-#x -- a real number
-#k -- a strictly positive number
-#
-#Return :
-#The sigmoid function with parameter k applied to x
-
+"""
+sig
+Arguments : x a real number and k a strictly positive number
+Return : the sigmoid function with parameter k applied to x
+"""
 def sig(x,k):
     return sig1(k*x)
 
-#relu10(x): ####################
-#
-#Arguments :
-#x -- a real number
-#
-#Return :
-#The basic ReLU function applied to x
-
+"""
+relu10
+Arguments : x a real number
+Return : the basic ReLU function applied to x
+"""
 def relu10(x):
     return max(x,0)
 
-#relu(x): ####################
-#
-#Arguments :
-#x -- a real number
-#a -- a real number
-#b -- a real number
-#
-#Return :
-#The ReLU function with parameters a and b applied to x
-
+"""
+relu
+Arguments : x, a, b three real numbers
+Return : the ReLU function with parameters (a;b) applied to x
+"""
 def relu(x,a,b):
     return a*relu10(x-b)
